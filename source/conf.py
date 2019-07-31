8a70b3409a8fea013514e386650ece0b887fffc0
jonny jerry <869064381@qq.com>
2019-07-31 12:01:45 +0800

    aa

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, jonny'
 author = 'jonny'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0。0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
